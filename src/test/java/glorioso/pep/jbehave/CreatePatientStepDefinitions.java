d72410703cad727b6e8d6df91994ba5a1248897b
leonardo.yvens <leoyvens@gmail.com>
2015-09-25 21:11:08 -0300

    put alias comment back

--- src/test/java/glorioso/pep/jbehave/CreatePatientStepDefinitions.java
+++ src/test/java/glorioso/pep/jbehave/CreatePatientStepDefinitions.java
@@ -2,6 +2,7 @@ package glorioso.pep.jbehave;
 
 import net.thucydides.core.annotations.Steps;
 
+import org.jbehave.core.annotations.Alias;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
@@ -19,11 +20,13 @@ public class CreatePatientStepDefinitions {
     }
 
     @When("the user enters the information from '$name'")
+    // @Alias("the user enters the information from <name>")
     public void whenTheUserFillsAndSubmitsTheFormWithInformationFrom(String name) {
     	receptionist.fill_and_submit(name);
     }
 
     @Then("they should see a confirmation of the creation of '$name'")
+    // @Alias("they should see a confirmation of the creation of <name>")
     public void thenTheyShouldSeeAConfirmationPageWithInformationFrom(String name) {
     	receptionist.should_see_confirmation_page(name);
     }
