8db149962c042eda1a3204a331edfc9eadfd5272
Joao Pedro Franco <francojoaopedro@gmail.com>
2015-10-02 21:57:58 -0300

    Arrumando estrutura para implementar cenarios de consulta do paciente

---
+++ src/main/java/glorioso/pep/pages/ReadPatientFormPage.java
@@ -0,0 +1,36 @@
+package glorioso.pep.pages;
+
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebElement;
+
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("http://www.google.com") //so que nao
+public class ReadPatientFormPage extends PageObject {
+
+    @FindBy(name="name_field")
+    private WebElement nameField;
+    
+    @FindBy(name="submit_button")
+    private WebElement submitButton;
+    
+    @FindBy(name="confirm_title")
+    private WebElement ConfirmAlertTitle;
+    
+    public void fill(String name) {
+    	nameField.sendKeys(name);
+    }
+    
+    public void submit() {
+    	submitButton.click();
+    }
+    
+    public String getConfirmedPatientName() {
+    	return ConfirmAlertTitle.getText();
+    }
+}
