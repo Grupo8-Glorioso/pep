6017a9ec8bf0b3f46d04354bb5c708b3d5aea95d
Ticolol <tico.amendola1@gmail.com>
2015-10-16 15:59:47 -0300

    Modelo de interface para apresentação

--- src/main/java/glorioso/pep/model/entity/Contact.java
+++ src/main/java/glorioso/pep/model/entity/Contact.java
@@ -1,5 +1,32 @@
 package glorioso.pep.model.entity;
 
-public class Contact {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Contact {
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public List<String> getTypes() {
+	    List<String> tipVal = new ArrayList<String>();
+	    tipVal.add(new String("Email"));
+	    tipVal.add(new String("Endereço"));
+	    tipVal.add(new String("Telefone Residencial"));
+	    tipVal.add(new String("Telefone Comercial"));
+	    tipVal.add(new String("Celular"));
+	    return tipVal;
+	}
+	
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	private String type;
+	private String info;
 }
--- src/main/java/glorioso/pep/model/entity/Patient.java
+++ src/main/java/glorioso/pep/model/entity/Patient.java
@@ -5,9 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Patient implements Serializable {
-	
-	public static Patient patient = new Patient();
-	
+		
 	// necessario para o eclipse nao reclamar
 	private static final long serialVersionUID = 2L;
 	
@@ -17,42 +15,73 @@ public class Patient implements Serializable {
 	public void setIdPatient(long idPatient) {
 		this.idPatient = idPatient;
 	}
+	
 	public long getIdPatientOrg() {
 		return idPatientOrg;
 	}
 	public void setIdPatientOrg(long idPatientOrg) {
 		this.idPatientOrg = idPatientOrg;
 	}
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getMotherName() {
 		return motherName;
 	}
 	public void setMotherName(String motherName) {
-		this.motherName = motherName;
+		this.motherName = motherName; 
 	}
+	
 	public String getGender() {
 		return gender;
 	}
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+	public List<String> getGenders() {
+	    List<String> genVal = new ArrayList<String>();
+	    genVal.add(new String("Masculino"));
+	    genVal.add(new String("Feminino"));
+	    genVal.add(new String("Outro"));
+	    return genVal;
+	}
+	
 	public String getCPF() {
 		return CPF;
 	}
 	public void setCPF(String cPF) {
 		CPF = cPF;
 	}
+	
+	
 	public List<Contact> getContactList() {
 		return contactList;
 	}
 	public void setContactList(List<Contact> contactList) {
 		this.contactList = contactList;
 	}
+	public String getContactType() {
+		return c.getType();
+	}
+	public void setContactType(String type) {
+		c.setType(type);;
+	}
+	public String getContactInfo() {
+		return c.getInfo();
+	}
+	public void setContactInfo(String info) {
+		c.setInfo(info);
+	}
+	public List<String> getContactTypes(){
+		return c.getTypes();
+	}
+	
+	
 	private long idPatient;
 	private long idPatientOrg;
 	
@@ -60,5 +89,19 @@ public class Patient implements Serializable {
 	private String motherName;
 	private String gender;
 	private String CPF;
+	private Contact c = new Contact();
 	private List<Contact> contactList = new ArrayList<Contact>();
+	
+	
+	public String Insert(){
+		System.out.println("Chamou!");
+		return "confCadPacientes";
+	}
+	
+	public void CreateContact(){
+		System.out.println("Mensagem recebida: " + c.getType() + " - " + c.getInfo());
+		contactList.add(c);
+		System.out.println(contactList);
+		//contactList.add(contact);
+	}
 }
