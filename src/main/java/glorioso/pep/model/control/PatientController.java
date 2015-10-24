1f99b129404051aaf7c998a161d4afd26794de85
Heitor Siqueira <hrasiq@gmail.com>
2015-10-23 22:55:10 -0200

    Refactored PatientController insert() method.

--- src/main/java/glorioso/pep/model/control/PatientController.java
+++ src/main/java/glorioso/pep/model/control/PatientController.java
@@ -13,15 +13,22 @@ import glorioso.pep.model.entity.Patient;
 public class PatientController {
 	private Patient p;
 	
-	public Patient getP() {
-		return p;
-	}
-
-	public void setP(Patient p) {
-		this.p = p;
-	}
-
-	public void insert() {
+	public void insert(String name, String gender, String CPF, 
+			String motherName, String fatherName, String address,
+			String neighborhood, String birthPlace, String zipCode,
+			String maritalStatus, String birthDate, String phoneNumber) {
+		p.setName(name);
+		p.setGender(gender);
+		p.setCPF(CPF);
+		p.setMotherName(motherName);
+		p.setFatherName(fatherName);
+		p.setAddress(address);
+		p.setNeighborhood(neighborhood);
+		p.setBirthPlace(birthPlace);
+		p.setZipCode(zipCode);
+		p.setMaritalStatus(maritalStatus);
+		p.setBirthDate(birthDate);
+		p.setPhoneNumber(phoneNumber);
 		try {
 			ConnectionSource cs = new JdbcConnectionSource("jdbc:sqlite:pep.db");
 			Dao<Patient,Integer> pd = DaoManager.createDao(cs, Patient.class);
