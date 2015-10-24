e388cc0887c7de3e3d752d8feea1f6704a19f651
Heitor Siqueira <hrasiq@gmail.com>
2015-10-23 22:31:37 -0200

    Added PatientController class.

---
+++ src/main/java/glorioso/pep/model/control/PatientController.java
@@ -0,0 +1,35 @@
+package glorioso.pep.model.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import glorioso.pep.model.entity.Patient;
+
+public class PatientController {
+	private Patient p;
+	
+	public Patient getP() {
+		return p;
+	}
+
+	public void setP(Patient p) {
+		this.p = p;
+	}
+
+	public void insert() {
+		try {
+			ConnectionSource cs = new JdbcConnectionSource("jdbc:sqlite:pep.db");
+			Dao<Patient,Integer> pd = DaoManager.createDao(cs, Patient.class);
+			pd.create(this.p);
+			cs.close();
+		} catch (SQLException | IOException e) {
+			System.err.printf("Patient insert failed (%s)", e.toString());
+			e.printStackTrace();
+		}
+	}
+}
